0a023f82eb15adcf2cccba2bddfe17454564b45f
William Sanchez <alejandrosanchez@hotmail.de>
2021-08-20 12:06:12 -0500

    InitialCommit

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            swaggerClient client = new("https://localhost:44320", new HttpClient());
+
+            var records = await client.WeatherForecastAsync();
+
+            foreach (var rec in records)
+            {
+                Console.WriteLine(rec.Summary);
+            }
+        }
+    }
+}
